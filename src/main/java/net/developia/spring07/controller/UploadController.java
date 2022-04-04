a731bde5f5f437c47d5c0bfaac5042ee81c12534
COM <COM@DESKTOP-TH36TT5>
2022-04-04 16:02:58 +0900

    file upload project

---
+++ src/main/java/net/developia/spring07/controller/UploadController.java
@@ -0,0 +1,20 @@
+package net.developia.spring07.controller;
+
+import javax.servlet.annotation.MultipartConfig;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class UploadController {
+
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+		log.info("upload form");
+	}
+	
+	
+}
