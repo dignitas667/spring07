110c5b013979912b400545dfb1868d514132fa1a
COM <COM@DESKTOP-TH36TT5>
2022-04-04 16:42:31 +0900

    file upload

--- src/main/java/net/developia/spring07/controller/UploadController.java
+++ src/main/java/net/developia/spring07/controller/UploadController.java
@@ -1,9 +1,10 @@
 package net.developia.spring07.controller;
 
-import javax.servlet.annotation.MultipartConfig;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
 
@@ -11,10 +12,17 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class UploadController {
 
-	@GetMapping("/uploadForm")
+	@GetMapping("uploadForm")
 	public void uploadForm() {
 		log.info("upload form");
 	}
 	
-	
+	@PostMapping("uploadFormAction")
+	public void uploadFormAction(MultipartFile[] uploadFile, Model model) {
+		for(MultipartFile multipartFile: uploadFile) {
+			log.info("-----------------------------------------");
+			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size : " + multipartFile.getSize());
+		}
+	}
 }
